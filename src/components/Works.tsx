a66219b4fa687d1c7296c43c9014e12c2c81f256
charltonkdev <charltonk.personal@gmail.com>
2024-07-10 11:38:49 +0800

    fix works

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -10,7 +10,7 @@ export default function Works() {
             </h3>
             {workItems.map((item) => (
                 <div key={item.id} className="flex md:flex-row flex-col justify-evenly container py-20 border-b-2 group">
-                    <div className="flex flex-col p-6 gap-2 justify-between">
+                    <div className="flex flex-grow flex-col p-6 gap-2 justify-between">
                         <div>
                             <h2 className="text-4xl font-semibold pb-3">{item.title}</h2>
                             <p className="pb-12">{item.description}</p>
@@ -31,12 +31,12 @@ export default function Works() {
                             Project Details <MdOutlineArrowOutward />
                         </button>
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col flex-grow">
                         <div className="w-full h-full overflow-hidden rounded-3xl">
-                            <Image src={item.img} alt={item.title} sizes="objectFit" 
-                            width={1000}
-                            height={1000}
-                            className="group-hover:scale-105 duration-300"/>
+                            <img src={item.img} alt={item.title} sizes="objectFit"
+                                width={800}
+                                height={800}
+                                className="group-hover:scale-105 duration-300" />
                         </div>
 
                     </div>
